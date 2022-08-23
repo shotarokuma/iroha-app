8764ab959c66d398f80e7f66067f0ac2c0f63eeb
Shotaro Kumagai <75811152+shotarokuma@users.noreply.github.com>
2022-08-23 07:12:18 -0700

    implement ui for charge asset

--- packages/client/components/AssetCard/index.tsx
+++ packages/client/components/AssetCard/index.tsx
@@ -1,5 +1,5 @@
+import { Role } from "../../../graphql/client";
 import React from "react";
-
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
@@ -8,9 +8,25 @@ import { CardActionArea } from "@mui/material";
 export interface CardProps {
   assetId: string;
   balance: string;
+  role: Role;
+  setTarget?: (target: string | null) => void;
+  setOpen?: (open: boolean) => void;
 }
 
-export const AssetCard: React.FC<CardProps> = ({ assetId, balance }) => {
+export const AssetCard: React.FC<CardProps> = ({
+  assetId,
+  balance,
+  role,
+  setTarget,
+  setOpen,
+}) => {
+  const handleClickCard = (): void => {
+    if (role === Role.Admin && setOpen && setTarget) {
+      setOpen(true);
+      setTarget(assetId);
+    }
+  };
+
   return (
     <Card
       style={{
@@ -18,7 +34,7 @@ export const AssetCard: React.FC<CardProps> = ({ assetId, balance }) => {
         width: "275px",
       }}
     >
-      <CardActionArea onClick={() => console.log("charge!!")}>
+      <CardActionArea onClick={handleClickCard}>
         <CardContent>
           <Typography variant="h5" gutterBottom>
             {assetId}
--- packages/client/pages/admin/index.tsx
+++ packages/client/pages/admin/index.tsx
@@ -1,11 +1,23 @@
-import { useGetAssetQuery } from "../../../graphql//client";
+import {
+  Role,
+  ChargeAssetInput,
+  useGetAssetQuery,
+  useChargeAssetMutation,
+} from "../../../graphql//client";
 import React from "react";
 import { NextPage } from "next";
+import { useForm } from "react-hook-form";
+import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
 import Grid from "@mui/material/Grid";
 import { useAdmin } from "../../hooks/admin";
 import { AssetCard } from "../../components/AssetCard";
 import Header from "../../components/Header";
+import FormController from "../../components/FormController";
 import Loading from "../../components/Loading";
 
 const Page: NextPage = () => {
@@ -16,9 +28,41 @@ const Page: NextPage = () => {
       },
     },
   });
+  const [open, setOpen] = React.useState<boolean>(false);
+  const handleClose = (): void => {
+    setOpen(false);
+  };
+  const [target, setTarget] = React.useState<string | null>(null);
   const { roots, onClickLogOut } = useAdmin();
+  const [chargeAssetMutation, { loading: chargeAssetLoading }] =
+    useChargeAssetMutation();
+  const { handleSubmit, control } = useForm<ChargeAssetInput>();
 
-  if (getAssetLoading) {
+  const getTarget = (): string | null => target;
+
+  const onSubmit = React.useCallback(
+    async (inputData: Partial<ChargeAssetInput>) => {
+      const assetId = getTarget();
+      if (!inputData.amount || !assetId) return;
+      try {
+        await chargeAssetMutation({
+          variables: {
+            input: {
+              asset: assetId,
+              amount: inputData.amount,
+            },
+          },
+        });
+        alert("charged your bit coin");
+        setOpen(false);
+      } catch (err) {
+        alert(err);
+      }
+    },
+    [chargeAssetMutation, getTarget]
+  );
+
+  if (getAssetLoading || chargeAssetLoading) {
     return <Loading />;
   }
 
@@ -31,12 +75,44 @@ const Page: NextPage = () => {
             data.getAsset.map((a: any, ind: number) => {
               return (
                 <Grid item key={ind} xs={3}>
-                  <AssetCard assetId={a.assetId} balance={a.balance} />
+                  <AssetCard
+                    assetId={a.assetId}
+                    balance={a.balance}
+                    role={Role.Admin}
+                    setTarget={setTarget}
+                    setOpen={setOpen}
+                  />
                 </Grid>
               );
             })}
         </Grid>
       </Container>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>{"Edit your data"}</DialogTitle>
+        <form>
+          <DialogContent
+            style={{
+              width: 500,
+            }}
+          >
+            <FormController
+              name="amount"
+              control={control}
+              label="input amount of bitcoin you want to charge"
+              fullWidth={true}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button
+              onClick={handleSubmit(onSubmit)}
+              variant="contained"
+              color="primary"
+            >
+              send
+            </Button>
+          </DialogActions>
+        </form>
+      </Dialog>
     </>
   );
 };
