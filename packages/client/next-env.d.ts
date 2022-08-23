56f05cffdeb839133a0f3acccb5c4dfc01cdf51a
Shotaro Kumagai <75811152+shotarokuma@users.noreply.github.com>
2022-08-23 05:23:30 -0700

    fix eslint for client side

--- packages/client/next-env.d.ts
+++ packages/client/next-env.d.ts
@@ -1,6 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference types="next" />
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference types="next/image-types/global" />
 
 // NOTE: This file should not be edited
