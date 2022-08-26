0b9c9945f531243cded538e7fa2681a2efaaf049
Shotaro Kumagai <75811152+shotarokuma@users.noreply.github.com>
2022-08-25 18:58:44 -0700

    implement auth for admin

--- packages/client/auth/admin.tsx
+++ packages/client/auth/admin.tsx
@@ -1,12 +1,19 @@
+import React from "react";
+import { useRouter } from "next/router";
 import client from "./apollo";
+import Loading from "../components/Loading";
+
+interface Props {
+  children: React.ReactNode;
+}
 
 export const storeToken = (token: string | null | undefined): void => {
   if (!token) return;
   localStorage.setItem("token", token);
 };
 
-export const redirectLogin = (url: string): boolean => {
-  if (url !== "/admin/login") {
+const redirectLogin = (url: string): boolean => {
+  if (typeof window !== "undefined" && url !== "/admin/login") {
     if (localStorage.getItem("token") === null) {
       window.location.href = "/admin/login";
       return false;
@@ -21,5 +28,14 @@ export const logout = async (): Promise<void> => {
     localStorage.removeItem("token");
   } catch (err) {
     alert(err);
+  }
+};
+
+export const Auth: React.FC<Props> = ({ children }) => {
+  const router = useRouter();
+  if (redirectLogin(router.pathname)) {
+    return <>{children}</>;
+  } else {
+    return <Loading />;
   }
 };
--- packages/client/pages/_app.tsx
+++ packages/client/pages/_app.tsx
@@ -1,25 +1,21 @@
 import { ApolloProvider } from "@apollo/client";
 import { AppProps } from "next/app";
 import Head from "next/head";
-import { useRouter } from "next/router";
 import React from "react";
 import { ThemeProvider } from "@mui/material/styles";
 
 import client from "../auth/apollo";
-import { redirectLogin } from "../auth/admin";
+import { Auth } from "../auth/admin";
 import theme from "../style/theme";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  const router = useRouter();
-  React.useEffect(() => {
-    redirectLogin(router.pathname);
-  }, []);
-
   return (
     <ApolloProvider client={client}>
       <Head>iroha-app</Head>
       <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
+        <Auth>
+          <Component {...pageProps} />
+        </Auth>
       </ThemeProvider>
     </ApolloProvider>
   );
