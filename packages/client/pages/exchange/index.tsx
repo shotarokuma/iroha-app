d1c8c4174d088eadcc395f73dc52eb385d0bdd35
Shotaro Kumagai <75811152+shotarokuma@users.noreply.github.com>
2022-09-21 14:31:56 -0700

    display images

--- packages/client/pages/exchange/index.tsx
+++ packages/client/pages/exchange/index.tsx
@@ -1,87 +1,48 @@
+import { Item } from "../../../graphql/client";
 import React from "react";
 import { NextPage } from "next";
+import Image from "next/image";
 // import { useRouter } from "next/router";
 import { useUser } from "../../hooks/user";
 import Container from "@mui/material/Container";
-import ListSubheader from "@mui/material/ListSubheader";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import ImageListItemBar from "@mui/material/ImageListItemBar";
 import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
 import PaymentIcon from "@mui/icons-material/Payment";
 import Header from "../../components/Header";
 
-const itemData = [
+const itemData: Item[] = [
   {
-    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
-    title: "Breakfast",
-    author: "@bkristastucchio",
-    rows: 2,
-    cols: 2,
-    featured: true,
+    img: "/img/item1.jpg",
+    item: "Candy",
+    price: "1.0",
   },
   {
-    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
-    title: "Burger",
-    author: "@rollelflex_graphy726",
+    img: "/img/item2.png",
+    item: "Alchool",
+    price: "10.0",
   },
   {
-    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
-    title: "Camera",
-    author: "@helloimnik",
+    img: "/img/item3.jpg",
+    item: "Carot",
+    price: "10.0",
   },
   {
-    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
-    title: "Coffee",
-    author: "@nolanissac",
-    cols: 2,
+    img: "/img/item4.jpg",
+    item: "Lettuce",
+    price: "10.0",
   },
   {
-    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
-    title: "Hats",
-    author: "@hjrc33",
-    cols: 2,
+    img: "/img/item5.jpg",
+    item: "Poteato",
+    price: "10.0",
   },
   {
-    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
-    title: "Honey",
-    author: "@arwinneil",
-    rows: 2,
-    cols: 2,
-    featured: true,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
-    title: "Basketball",
-    author: "@tjdragotta",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
-    title: "Fern",
-    author: "@katie_wasserman",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
-    title: "Mushrooms",
-    author: "@silverdalex",
-    rows: 2,
-    cols: 2,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
-    title: "Tomato basil",
-    author: "@shelleypauls",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
-    title: "Sea star",
-    author: "@peterlaster",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
-    title: "Bike",
-    author: "@southside_customs",
-    cols: 2,
+    img: "/img/item6.jpg",
+    item: "Travel",
+    price: "100",
   },
 ];
 
@@ -93,24 +54,28 @@ const Page: NextPage = () => {
       <Container component="main" maxWidth="lg">
         <ImageList>
           <ImageListItem key="Subheader" cols={2}>
-            <ListSubheader component="div">Select your item</ListSubheader>
+            <Typography
+              component="h2"
+              variant="h5"
+              textAlign="center"
+              style={{
+                marginTop: "30px",
+              }}
+            >
+              Select your item
+            </Typography>
           </ImageListItem>
-          {itemData.map((item) => (
-            <ImageListItem key={item.img}>
-              <img
-                src={`${item.img}?w=248&fit=crop&auto=format`}
-                srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                alt={item.title}
-                loading="lazy"
-              />
+          {itemData.map((i) => (
+            <ImageListItem
+              key={i.img}
+              style={{ width: "100%", height: "400px", position: "relative" }}
+            >
+              <Image src={i.img} alt={i.item} layout="fill" loading="lazy" />
               <ImageListItemBar
-                title={item.title}
-                subtitle={item.author}
+                title={i.item}
+                subtitle={i.price + " pt"}
                 actionIcon={
-                  <IconButton
-                    sx={{ color: "rgba(255, 255, 255, 0.54)" }}
-                    aria-label={`info about ${item.title}`}
-                  >
+                  <IconButton aria-label={i.item} color="secondary">
                     <PaymentIcon />
                   </IconButton>
                 }
