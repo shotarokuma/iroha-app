143b07f5d03a0061aea3a2ca26e1b56d9e8f75bb
Shotaro Kumagai <75811152+shotarokuma@users.noreply.github.com>
2022-09-24 16:58:08 -0700

    fix endpoint for api

--- packages/client/auth/apollo.ts
+++ packages/client/auth/apollo.ts
@@ -2,7 +2,7 @@ import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: process.env.SERVER_PORT,
+  uri: process.env.NEXT_PUBLIC_SERVER_PORT,
 });
 
 const authLink = setContext((_, { headers }) => {
